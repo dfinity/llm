a0ff7a3fea41cdc84eb3d1e9940b024073fa8bef
Islam El-Ashi <islam.elashi@dfinity.org>
2025-02-12 11:56:19 +0100

    feat: skeleton of rust library

---
+++ rust/src/lib.rs
@@ -0,0 +1,25 @@
+use candid::{CandidType, Principal};
+
+const LLM_CANISTER: &str = "bnz7o-iuaaa-aaaaa-qaaaa-cai";
+
+#[derive(CandidType, Clone)]
+struct Request {
+    prompt: String,
+    model: String,
+}
+
+pub async fn prompt<P: ToString>(prompt_str: P) -> String {
+    let llm_canister = Principal::from_text(LLM_CANISTER).expect("invalid canister id");
+
+    let res: (String,) = ic_cdk::call(
+        llm_canister,
+        "v0_prompt",
+        (Request {
+            prompt: prompt_str.to_string(),
+            model: "ds".to_string(),
+        },),
+    )
+    .await
+    .unwrap();
+    res.0
+}
