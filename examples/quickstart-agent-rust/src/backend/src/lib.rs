a64df24131632debdce9a6e372c2cb71bc119bfa
Islam El-Ashi <islam.elashi@dfinity.org>
2025-02-19 16:33:52 +0100

    Rename agent to quickstart agent

--- examples/quickstart-agent-rust/src/backend/src/lib.rs
+++ examples/quickstart-agent-rust/src/backend/src/lib.rs
@@ -1,11 +1,4 @@
-use candid::{CandidType, Principal};
-use ic_cdk::api::{call::call, caller};
-use ic_ledger_types::{
-    account_balance, AccountBalanceArgs, AccountIdentifier, Tokens, DEFAULT_SUBACCOUNT,
-    MAINNET_LEDGER_CANISTER_ID,
-};
-use ic_llm::{ChatMessage, Model, Role};
-use std::fmt;
+use ic_llm::{ChatMessage, Model};
 
 #[ic_cdk::update]
 async fn prompt(prompt_str: String) -> String {
