0e8b57c40bb3aeb92a3e58a841d7c64e5e39da59
Islam El-Ashi <islam.elashi@dfinity.org>
2025-06-18 10:11:45 +0200

    chore: update quickstart to use latest typescript library (#43)

--- examples/quickstart-agent-typescript/src/backend/index.ts
+++ examples/quickstart-agent-typescript/src/backend/index.ts
@@ -1,6 +1,35 @@
 import { IDL, update } from "azle";
-import { chat_message as ChatMessageIDL } from "azle/canisters/llm/idl";
 import * as llm from "@dfinity/llm";
+
+const ToolCallArgument_IDL = IDL.Record({
+  name: IDL.Text,
+  value: IDL.Text,
+});
+
+const FunctionCall_IDL = IDL.Record({
+  name: IDL.Text,
+  arguments: IDL.Vec(ToolCallArgument_IDL),
+});
+
+const ToolCall_IDL = IDL.Record({
+  id: IDL.Text,
+  function: FunctionCall_IDL,
+});
+
+const AssistantMessage_IDL = IDL.Record({
+  content: IDL.Opt(IDL.Text),
+  tool_calls: IDL.Vec(ToolCall_IDL),
+});
+
+export const ChatMessage_IDL = IDL.Variant({
+  user: IDL.Record({
+    content: IDL.Text,
+  }),
+  assistant: AssistantMessage_IDL,
+  system: IDL.Record({
+    content: IDL.Text,
+  }),
+});
 
 export default class {
   @update([IDL.Text], IDL.Text)
@@ -8,8 +37,11 @@ export default class {
     return await llm.prompt(llm.Model.Llama3_1_8B, prompt);
   }
 
-  @update([IDL.Vec(ChatMessageIDL)], IDL.Text)
+  @update([IDL.Vec(ChatMessage_IDL)], IDL.Text)
   async chat(messages: llm.ChatMessage[]): Promise<string> {
-    return await llm.chat(llm.Model.Llama3_1_8B, messages);
+    return (
+      (await llm.chat(llm.Model.Llama3_1_8B).withMessages(messages).send())
+        .message.content?.[0] || ""
+    );
   }
 }
--- examples/quickstart-agent-typescript/src/frontend/src/canisters/agent-backend/agent-backend.did.d.ts
+++ examples/quickstart-agent-typescript/src/frontend/src/canisters/agent-backend/agent-backend.did.d.ts
@@ -2,14 +2,30 @@ import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 import type { IDL } from '@dfinity/candid';
 
-export interface ChatMessage {
-  'content' : string,
-  'role' : { 'user' : null } |
-    { 'assistant' : null } |
-    { 'system' : null },
-}
+export type ChatMessage = { 'user' : { 'content' : string } } |
+{
+  'assistant' : {
+    'content' : [] | [string],
+    'tool_calls' : Array<
+      {
+        'id' : string,
+        'function' : {
+          'name' : string,
+          'arguments' : Array<{ 'value' : string, 'name' : string }>,
+        },
+      }
+    >,
+  }
+} |
+{ 'system' : { 'content' : string } };
+
 export interface _SERVICE {
-  'chat' : ActorMethod<[Array<ChatMessage>], string>,
+  'chat' : ActorMethod<
+    [
+      Array<ChatMessage>,
+    ],
+    string
+  >,
   'prompt' : ActorMethod<[string], string>,
 }
 export declare const idlFactory: IDL.InterfaceFactory;
